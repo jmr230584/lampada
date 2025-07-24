191179e24df84163a6a2bde9afd932b2d82c4c07
José Mateus <mateusjoseph@gmail.com>
2025-07-24 11:34:02 -0300

    atualizando .html e .js

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-
+let lampadaSalaLigada = false;
+let lampadaCozinhaLigada = false;
 
 function ligarLampadaSala(){
-    
 
+    if(lampadaSalaLigada == false){
+        document.getElementById("lamp-sala").classList.add("acesa");
+        document.getElementById("bt-sala").textContent = "Desligar"
+        lampadaSalaLigada = true; 
+    } else {
+        document.getElementById("lamp-sala").classList.remove("acesa");
+        document.getElementById("bt-sala").textContent = "Ligar"
+        lampadaSalaLigada = false; 
+    }   
 }
 
 function ligarLampadaCozinha(){
